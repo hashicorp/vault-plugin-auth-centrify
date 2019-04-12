c5ae154b8c40732177bcc4cec86c7edcadb204d7
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-04-12 18:13:41 -0400

    Switch to go modules

--- cmd/vault-plugin-auth-centrify/main.go
+++ cmd/vault-plugin-auth-centrify/main.go
@@ -6,17 +6,17 @@ import (
 	log "github.com/hashicorp/go-hclog"
 
 	centrify "github.com/hashicorp/vault-plugin-auth-centrify"
-	"github.com/hashicorp/vault/helper/pluginutil"
-	"github.com/hashicorp/vault/logical/plugin"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
 )
 
 func main() {
-	apiClientMeta := &pluginutil.APIClientMeta{}
+	apiClientMeta := &api.APIClientMeta{}
 	flags := apiClientMeta.FlagSet()
 	flags.Parse(os.Args[1:])
 
 	tlsConfig := apiClientMeta.GetTLSConfig()
-	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
 	err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: centrify.Factory,
